464addc1d9754740eeb4248acf8251b1f4676692
K Thuhlo-Cakeni <223531482@tut4life.ac.za>
2026-05-13 02:33:16 +0200

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,10 @@ app = FastAPI()
 
 
 origins = [
-    "https://studentdoctor.co.za"
+    "https://studentdoctor.co.za",
+    "https://www.studentdoctor.co.za",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 
